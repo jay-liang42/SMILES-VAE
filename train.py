da54f8228af9e57be9e5da901c9fc5a848fa0041
Jay Liang <115037372+jay-liang42@users.noreply.github.com>
2026-03-04 23:49:32 -0800

    gradient clipping

--- train.py
+++ train.py
@@ -146,6 +146,7 @@ for epoch in range(cfg.epochs):
         loss = recon_loss + cfg.beta * kl
         optimizer.zero_grad()
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
         total_loss += loss.item()
         total_kl += kl.item()
