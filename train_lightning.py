0d42436fced767fc29067eae664ee3220a2151e1
Jay Liang <115037372+jay-liang42@users.noreply.github.com>
2026-03-11 03:37:19 -0700

    Create train_lightning.py

--- train_lightning.py
+++ train_lightning.py
@@ -6,6 +6,28 @@ from lightning_model import SMILESVAE
 from data_module import SMILESDataModule
 from config import Config
 
+import argparse
+from config import config
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--z_dim", type=int)
+parser.add_argument("--h_dim", type=int)
+parser.add_argument("--emb_dim", type=int)
+parser.add_argument("--beta", type=float)
+parser.add_argument("--lr", type=float)
+
+args = parser.parse_args()
+
+if args.z_dim:
+    config.z_dim = args.z_dim
+if args.h_dim:
+    config.h_dim = args.h_dim
+if args.emb_dim:
+    config.emb_dim = args.emb_dim
+if args.beta:
+    config.beta = args.beta
+if args.lr:
+    config.lr = args.lr
 
 def main():
 
