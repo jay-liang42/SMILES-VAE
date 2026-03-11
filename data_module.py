50c0f4a9a81efa49dc11d763563326d50f4a02bb
Jay Liang <115037372+jay-liang42@users.noreply.github.com>
2026-03-11 12:15:41 -0700

    Update data_module.py

--- data_module.py
+++ data_module.py
@@ -1,8 +1,7 @@
 import lightning as L
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
 from data_utils import SmilesDataset, build_vocab
-from config import Config
 
 
 class SMILESDataModule(L.LightningDataModule):
@@ -19,28 +18,40 @@ class SMILESDataModule(L.LightningDataModule):
         with open(self.smiles_file) as f:
             smiles = [line.strip() for line in f if line.strip()]
 
+        # Build vocabulary
         self.stoi, self.itos = build_vocab(smiles)
 
-        self.dataset = SmilesDataset(
+        dataset = SmilesDataset(
             self.smiles_file,
             self.stoi,
             self.max_len
         )
 
+        # 90/10 split
+        train_size = int(0.9 * len(dataset))
+        val_size = len(dataset) - train_size
+
+        self.train_dataset, self.val_dataset = random_split(
+            dataset,
+            [train_size, val_size]
+        )
+
     def train_dataloader(self):
 
         return DataLoader(
-            self.dataset,
+            self.train_dataset,
             batch_size=self.batch_size,
             shuffle=True,
-            num_workers=4
+            num_workers=4,
+            persistent_workers=True
         )
 
     def val_dataloader(self):
 
         return DataLoader(
-            self.dataset,
+            self.val_dataset,
             batch_size=self.batch_size,
             shuffle=False,
-            num_workers=4
+            num_workers=4,
+            persistent_workers=True
         )
