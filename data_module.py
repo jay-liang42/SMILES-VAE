fa837bf2b4efbf2a277587087f96fb1ab4e9ab30
Jay Liang <115037372+jay-liang42@users.noreply.github.com>
2026-03-11 20:37:14 -0700

    Update data_module.py

--- data_module.py
+++ data_module.py
@@ -1,8 +1,8 @@
 import torch
 from torch.utils.data import DataLoader, random_split
-import pytorch_lightning as pl  
+import pytorch_lightning as pl
 
-from data_utils import SmilesDataset, build_vocab, PAD_TOKEN
+from data_utils import SmilesDataset, build_vocab
 
 
 class SMILESDataModule(pl.LightningDataModule):
@@ -12,7 +12,6 @@ class SMILESDataModule(pl.LightningDataModule):
         self.batch_size = batch_size
         self.max_len = max_len
 
-        # will be set after setup
         self.stoi = None
         self.itos = None
         self.train_dataset = None
@@ -33,28 +32,14 @@ class SMILESDataModule(pl.LightningDataModule):
             self.max_len
         )
 
-        # Split 90/10
+        # Train/validation split (90/10)
         train_size = int(0.9 * len(dataset))
         val_size = len(dataset) - train_size
+
         self.train_dataset, self.val_dataset = random_split(
-            dataset, [train_size, val_size]
+            dataset,
+            [train_size, val_size]
         )
-
-    def _one_hot_flattened(self, batch):
-        """
-        Converts a batch of sequences (LongTensor) to flattened one-hot FloatTensor.
-        Input: [batch, seq_len]
-        Output: [batch, seq_len * vocab_size]
-        """
-        # batch is a list of LongTensors from SmilesDataset
-        x = torch.stack(batch)  # [batch, seq_len]
-        batch_size, seq_len = x.shape
-        vocab_size = len(self.stoi)
-
-        # One-hot encoding
-        x_oh = torch.nn.functional.one_hot(x, num_classes=vocab_size).float()
-        x_flat = x_oh.view(batch_size, seq_len * vocab_size)  # flattened
-        return x_flat
 
     def train_dataloader(self):
         return DataLoader(
@@ -62,8 +47,7 @@ class SMILESDataModule(pl.LightningDataModule):
             batch_size=self.batch_size,
             shuffle=True,
             num_workers=4,
-            persistent_workers=True,
-            collate_fn=self._one_hot_flattened
+            persistent_workers=True
         )
 
     def val_dataloader(self):
@@ -72,6 +56,5 @@ class SMILESDataModule(pl.LightningDataModule):
             batch_size=self.batch_size,
             shuffle=False,
             num_workers=4,
-            persistent_workers=True,
-            collate_fn=self._one_hot_flattened
+            persistent_workers=True
         )
