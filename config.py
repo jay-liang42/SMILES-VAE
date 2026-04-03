9fdce694f26b3fb82878f1994bd781efa5b51042
Jay Liang <115037372+jay-liang42@users.noreply.github.com>
2026-04-03 13:25:29 -0700

    beta 0.1

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class Config:
     # Maximum token length for SMILES sequences.
     # Longer molecules get truncated/padded to this size.
 
-    beta: float = 0.0
+    beta: float = 0.1
     # Weight of KL-divergence term in VAE loss.
     # Higher -> stronger latent compression
     # Lower -> better reconstruction
