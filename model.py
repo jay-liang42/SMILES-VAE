066119abdfdff3c92755f83cb2d5e41f6986fa56
Jay Liang <115037372+jay-liang42@users.noreply.github.com>
2026-04-11 13:34:00 -0700

    Update model.py

--- model.py
+++ model.py
@@ -41,7 +41,7 @@ class SmilesVAE(nn.Module):
         eps = torch.randn_like(std)
         return mu + eps * std
 
-    def decode(self, z, x, teacher_forcing_ratio=1.0):
+    def decode(self, z, x, teacher_forcing_ratio=0.3):
         h = self.fc_z(z).unsqueeze(0)
         batch_size, seq_len = x.size()
     
@@ -93,7 +93,7 @@ class SmilesVAE(nn.Module):
         x_input = x[:, :-1]    # input to decoder
         x_target = x[:, 1:]    # expected output
 
-        logits = self.decode(z, x_input, teacher_forcing_ratio=1.0)
+        logits = self.decode(z, x_input, teacher_forcing_ratio=0.3)
 
         return logits, mu, logvar, x_target
 
